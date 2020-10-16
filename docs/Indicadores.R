859a6e829af624f6586aedb3c2d744dd35c16433
demcortillas <emilianodecortillas@gmail.com>
2020-10-16 12:41:10 -0300

    readme y scripts

--- docs/Indicadores.R
+++ docs/Indicadores.R
@@ -1,7 +1,3 @@
-MAT
-PEJH12
-CANT_PER
-
 merge1<-merge(x=MAT,y=PEJH12,x.by="GEOCODIGO",y.by="GEOCODIGO")
 merge2<-merge(x=merge1,y=CANT_PER,x.by="GEOCODIGO",y.by="GEOCODIGO")
 
@@ -10,3 +6,5 @@ merge2<-merge(x=merge1,y=CANT_PER,x.by="GEOCODIGO",y.by="GEOCODIGO")
 FINAL_SHP <- merge(x = Pto_varas_sp, y = merge2, by.x="GEOCODIGO",by.y="GEOCODIGO") ### JOIN
 writeOGR(obj=FINAL_SHP, dsn="out", layer="FINAL", driver="ESRI Shapefile",overwrite_layer = TRUE)
 
+#Pto_varas1 <- merge(x = Pto_varas_sp, y = data, by.x="GEOCODIGO",by.y="GEOCODIGO") ### JOIN
+#writeOGR(obj=Pto_varas1, dsn="out", layer="Pto_varas_no-project", driver="ESRI Shapefile",overwrite_layer = TRUE)
